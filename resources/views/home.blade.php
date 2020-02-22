1b40aae4d4c33ae9181217c9edc3e71d925af99e
dodi <dodinovembri32@gmail.com>
2020-02-23 02:13:27 +0700

    logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
                         <div class="header-row">
                             <div class="header-logo">
                                 <a href="demo-business-consulting.html">
-                                    <img alt="Porto" width="82" height="40" src="{{asset('tmp/img/logo-default-slim-dark.png')}}">
+                                    <img alt="Porto" width="82" height="40" src="{{asset('tmp/img/logo-zedete-2.png')}}">
                                 </a>
                             </div>
                         </div>
